bff5774e6408659c191ab87559e21968c48471fb
alexeys6969 <alexshashin.2019@gmail.com>
2026-03-03 17:15:16 +0500

    конейц пары

--- app/src/main/java/com/example/singin_shashin/LogInActivity.java
+++ app/src/main/java/com/example/singin_shashin/LogInActivity.java
@@ -1,24 +1,48 @@
 package com.example.singin_shashin;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TextView bthOpenSingIn = findViewById(R.id.btn_open_sing_in);
+        bthOpenSingIn.setOnClickListener(v -> {
+            Intent SingIn = new Intent(this, RegInActivity.class);
+            startActivity(SingIn);
+        });
+
+        Button bthLogIn = findViewById(R.id.btn_log_in);
+        bthLogIn.setOnClickListener(v -> {
+            TextView etEmail = findViewById(R.id.et_email);
+            TextView etPassword = findViewById(R.id.et_password);
+
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            if(email.isEmpty()) {
+                Toast.makeText(this, "Не указана почта пользователя", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(password.isEmpty()) {
+                Toast.makeText(this, "Не указан пароль пользователя", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Toast.makeText(this, "Пользователь авторизован", Toast.LENGTH_SHORT).show();
         });
     }
 }
