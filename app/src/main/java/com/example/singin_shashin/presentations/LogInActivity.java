f4a250493a6320ddac030f83f2bf4ec2b21da3bf
alexeys6969 <alexshashin.2023gmail.com>
2026-03-06 14:40:29 +0500

    Взаимодействие с , авторизация пользователя. Шашин

--- app/src/main/java/com/example/singin_shashin/presentations/LogInActivity.java
+++ app/src/main/java/com/example/singin_shashin/presentations/LogInActivity.java
@@ -1,14 +1,21 @@
 package com.example.singin_shashin.presentations;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.singin_shashin.datas.apis.UserLogin;
+import com.example.singin_shashin.domains.callbacks.MyResponseCallback;
+import com.example.singin_shashin.domains.models.User;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.singin_shashin.R;
+import com.example.singin_shashin.datas.common.CheckInternet;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -39,7 +46,34 @@ public class LogInActivity extends AppCompatActivity {
                 Toast.makeText(this, "Не указан пароль пользователя", Toast.LENGTH_SHORT).show();
                 return;
             }
-            Toast.makeText(this, "Пользователь авторизован", Toast.LENGTH_SHORT).show();
+            RequestUserLogin(email, password);
         });
+    }
+
+    public void RequestUserLogin(String email, String password) {
+        Context context = this;
+        CheckInternet checkInternet = new CheckInternet(this);
+        User User = new User();
+        User.email = email;
+        User.password = password;
+
+        UserLogin RequestUserLogin = new UserLogin(
+                User,
+                checkInternet,
+                new MyResponseCallback() {
+                    @Override
+                    public void onCompile(String result) {
+                        Log.d("USER LOGIN", result);
+                        Toast.makeText(context, "Успешная авторизация пользователя", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("USER LOGIN", error);
+                        Toast.makeText(context, "Авторизация не увенчалась успехом", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+        RequestUserLogin.execute();
     }
 }
